d18efb871405b13dbd69c966596855dc7e0f186c
PreethamYerragudi <preetham.yerragudi@gmail.com>
2025-07-27 19:32:14 +0000

    LeetCode Solution for Problem #861

---
+++ Medium/861_score-after-flipping-matrix.py
@@ -0,0 +1,24 @@
+# Problem 861: Score After Flipping Matrix
+# Difficulty: Medium
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        """
+            U:
+                I: 2D array
+                O: 2D array
+                C: N/A
+                E: An empty image -> an empty array
+            P:
+                loop through every row, invert it and then flip it
+            I:
+        """
+        flippedImage = []
+        for row in image:
+            flippedRow = []
+            row = list(map(lambda x: int(not x), row))
+            for col in range(len(image[0]) - 1, -1, -1):
+                flippedRow.append(row[col])
+            flippedImage.append(flippedRow)
+        return flippedImage
+
+                
