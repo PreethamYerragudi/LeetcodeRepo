4ef8ad99b77562d777d11ec3c997abb84bf588d1
PreethamYerragudi <preetham.yerragudi@gmail.com>
2025-07-27 19:32:17 +0000

    LeetCode Solution for Problem #326

---
+++ Easy/326_power-of-three.py
@@ -0,0 +1,8 @@
+# Problem 326: Power of Three
+# Difficulty: Easy
+import math
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        while n > 1:
+            n = n / 3
+        return n == 1
