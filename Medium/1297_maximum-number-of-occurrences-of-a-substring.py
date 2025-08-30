e89ad76446b67ed1c25ea39dc5b003751397e7e5
PreethamYerragudi <preetham.yerragudi@gmail.com>
2025-08-30 20:31:54 +0000

    LeetCode solution for Problem #1297

---
+++ Medium/1297_maximum-number-of-occurrences-of-a-substring.py
@@ -0,0 +1,8 @@
+# Problem 1297: Maximum Number of Occurrences of a Substring
+# Difficulty: Medium
+class Solution:
+    def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
+        count = collections.Counter(s[i:i + minSize] for i in range(len(s) - minSize + 1))
+        return max([count[w] for w in count if len(set(w)) <= maxLetters] + [0])
+
+                
