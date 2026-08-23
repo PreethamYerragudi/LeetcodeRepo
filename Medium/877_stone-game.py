638d8371876eed69f4a26a426a6cf1717f634226
Preetham Yerragudi <preetham.yerragudi@gmail.com>
2026-08-22 23:59:34 -0400

    LeetCode solution for Problem #877

---
+++ Medium/877_stone-game.py
@@ -0,0 +1,15 @@
+# Problem 877: Stone Game
+# Difficulty: Medium
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        @lru_cache(maxsize=None)
+        def solve(i, j):
+            if i == j:
+                return piles[i]
+            return max(
+                piles[i] - solve(i + 1, j),
+                piles[j] - solve(i, j - 1)
+            )
+        return solve(0, len(piles) - 1) >= 0
+
+        
